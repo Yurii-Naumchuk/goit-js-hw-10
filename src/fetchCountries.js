39a281231dd3d2c599d47d06cea835b1ad72822d
Yurii-Naumchuk <uranaumchuk7@gmail.com>
2022-08-20 01:14:10 +0300

    3

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+// fetchCountries() => {
+// fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // console.log("data", data);
+//       if (e.type === "submit") {
+//         updateUi(data, pageSize);
+//       };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
+import {fetchCountries} from './fetchCountries';
+const debounce = require('lodash.debounce');
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+Notiflix.Notify.init({
+    fontSize: '28px',
+    width: '400px',
+    borderRadius: '40px',
+    });
+
+const searchCountry = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+const KEY = "fe54e78640174c269f1b558d40009301";
+const BASE_URL = "https://restcountries.com/v2";
+
+// https://restcountries.com/v2/name/{name}
+
+
+searchCountry.addEventListener('input',debounce(onSearchCountry,DEBOUNCE_DELAY));
