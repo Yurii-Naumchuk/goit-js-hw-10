f4449a4540bb4eac3de1914fe5566fb103464cbb
Yurii-Naumchuk <uranaumchuk7@gmail.com>
2022-08-20 04:48:20 +0300

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,13 @@
-// fetchCountries() => {
-// fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       // console.log("data", data);
-//       if (e.type === "submit") {
-//         updateUi(data, pageSize);
-//       };
+import Notiflix from 'notiflix'
+const BASE_URL = "https://restcountries.com/v3.1"
+
+export const fetchCountries = name => {
+    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => { 
+        if (!response.ok) {
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+            return Promise.reject('Oшибка 404') ;
+        }  
+        return response.json();
+    })
+};
